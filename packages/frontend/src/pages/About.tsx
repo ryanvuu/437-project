ee23e81e23b2830a74c443cfa9a3c6daeaab7bb5
ryanvuu <ryanvu077@gmail.com>
2025-05-22 02:21:58 -0700

    add about static page

--- packages/frontend/src/pages/About.tsx
+++ packages/frontend/src/pages/About.tsx
@@ -1,20 +1,20 @@
 import Navbar from "../Navbar";
-// import "../styles/about.css";
+import "../styles/about.css";
 
 export function About() {
   return (
     <div>
-      <h1 className="about-h">Music Recommender</h1>
+      <h1 className="h1-about">Music Recommender</h1>
       <div id="questions-container">
         <div className="question">
-          <h2 className="about-h">What is Music Recommender?</h2>
-          <p className="about-p">This is a paragraph to explain what music recommender is. What is its mission... to be completed when
+          <h2 className="h2-about">What is Music Recommender?</h2>
+          <p className="p-about">This is a paragraph to explain what music recommender is. What is its mission... to be completed when
             features get implemented.</p>
         </div>
 
         <div className="question">
-          <h2 className="about-h">How do I get started?</h2>
-          <p className="about-p">This is a paragraph to explain how to get started with using the music recommending application.
+          <h2 className="h2-about">How do I get started?</h2>
+          <p className="p-about">This is a paragraph to explain how to get started with using the music recommending application.
             When features get added, will mention how to filter, what it does, etc.</p>
         </div>
       </div>
