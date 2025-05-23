bec93894703d85609f37fe46417624fb46c36efc
ryanvuu <ryanvu077@gmail.com>
2025-05-22 21:26:05 -0700

    add profile page, can change display name

--- packages/frontend/src/pages/Profile.tsx
+++ packages/frontend/src/pages/Profile.tsx
@@ -1,10 +1,65 @@
 import Navbar from "../Navbar";
+import "../styles/profile.css";
+import logo from "../images/logo.png";
+import { ModalDisplayName } from "../ModalDisplayName";
+import { useState } from "react";
 
 export function Profile() {
+  const [isDisplayOpen, setIsDisplayOpen] = useState(false);
+  const [displayName, setDisplayName] = useState("User12345");
+
+  function openModal() {
+    setIsDisplayOpen(true);
+  }
+
+  function closeModal() {
+    setIsDisplayOpen(false);
+  }
+
   return (
     <div>
-      <h1>Profile</h1>
+      <div className="pic-name">
+        <img className="profile-pic" src={logo} alt="" height="200" width="200"></img>
 
+        <div className="desktop-prof">
+          <h1 className="h1-profile">{displayName}</h1>
+
+          <div className="profile-container">
+
+            <div className="profile-item genre-favs">
+              Genre Favorites
+            </div>
+
+            <div className="edit-display-genre-btns">
+
+              <button
+                className="open-prof-modal-btn"
+                onClick={openModal}>
+                  Edit Display Name
+              </button>
+
+              <button
+                className="open-prof-modal-btn">
+                Edit Favorite Genres
+              </button>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+      
+      
+
+
+      <ModalDisplayName 
+        headerLabel="Edit Display Name"
+        currentName={displayName}
+        isOpen={isDisplayOpen}
+        onCloseRequested={closeModal}
+        onDisplayNameSet={setDisplayName}
+      />
       <Navbar />
     </div>
   )
