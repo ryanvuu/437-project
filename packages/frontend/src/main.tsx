8e2eefd5fc75d30f9da9771c3388fef535ec2896
ryanvuu <ryanvu077@gmail.com>
2025-06-11 19:00:22 -0700

    remove react query dev tools

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import App from "./App";
 import { BrowserRouter } from "react-router";
 import { createRoot } from "react-dom/client";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./styles/tokens.css";
 
@@ -13,7 +12,6 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools />
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
